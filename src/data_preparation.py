700b1883a828e54ddeab788e02f440d55b412ea8
Jasper <jfuelle@smail.uni-koeln.de>
2023-11-30 17:52:58 +0100

    site_characteristics, converting times in timezone

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import ast
-from datetime import datetime
-
 
 csv_file_path = 'charging_sessions.csv'
 
@@ -30,11 +28,18 @@ def parse_user_inputs(row):
         return []
     
 
+# Funktion zur Umwandlung der Zeitzone
+def convert_timezone(time):
+    if pd.notna(time):
+        return time.tz_convert('America/Los_Angeles')
+    return time
+
+
 # Konvertieren der Datentypen in charging_sessions
 df['id'] = df['id'].astype(str)
-df['connectionTime'] = pd.to_datetime(df['connectionTime'], utc=True)
-df['disconnectTime'] = pd.to_datetime(df['disconnectTime'], utc=True)
-df['doneChargingTime'] = pd.to_datetime(df['doneChargingTime'], utc=True)
+df['connectionTime'] = pd.to_datetime(df['connectionTime'], utc=True).apply(convert_timezone)
+df['disconnectTime'] = pd.to_datetime(df['disconnectTime'], utc=True).apply(convert_timezone)
+df['doneChargingTime'] = pd.to_datetime(df['doneChargingTime'], utc=True).apply(convert_timezone)
 df['kWhDelivered'] = df['kWhDelivered'].astype(float)
 df['sessionID'] = df['sessionID'].astype(str)
 df['siteID'] = df['siteID'].astype(str)
@@ -42,6 +47,10 @@ df['spaceID'] = df['spaceID'].astype(str)
 df['stationID'] = df['stationID'].astype(str)
 df['timezone'] = df['timezone'].astype(str)
 df['userID'] = df['userID'].astype(str)
+
+df['connectionTime'] = df['connectionTime'].apply(convert_timezone)
+df['disconnectTime'] = df['disconnectTime'].apply(convert_timezone)
+df['doneChargingTime'] = df['doneChargingTime'].apply(convert_timezone)
     
 
 # Löscht Spalte Unnamed und anschließend alle Duplikate    
@@ -63,9 +72,9 @@ user_inputs_df['WhPerMile'] = user_inputs_df['WhPerMile'].astype(float)
 user_inputs_df['kWhRequested'] = user_inputs_df['kWhRequested'].astype(float)
 user_inputs_df['milesRequested'] = user_inputs_df['milesRequested'].astype(float)
 user_inputs_df['minutesAvailable'] = user_inputs_df['minutesAvailable'].astype(float)
-user_inputs_df['modifiedAt'] = pd.to_datetime(user_inputs_df['modifiedAt'], utc=True)
 user_inputs_df['paymentRequired'] = user_inputs_df['paymentRequired'].astype(bool)
-user_inputs_df['requestedDeparture'] = pd.to_datetime(user_inputs_df['requestedDeparture'], utc=True)
+user_inputs_df['modifiedAt'] = pd.to_datetime(user_inputs_df['modifiedAt'], utc=True).apply(convert_timezone)
+user_inputs_df['requestedDeparture'] = pd.to_datetime(user_inputs_df['requestedDeparture'], utc=True).apply(convert_timezone)
 
 
 # Duplikate in user_inputs_df löschen
