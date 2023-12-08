c5a1c52137ff336ff815c6dd81a07466a983fb73
Jasper <jfuelle@smail.uni-koeln.de>
2023-12-08 12:25:13 +0100

    komische spaceID analysiert

--- src/SiteCharacteristics/other_characteristics.py
+++ src/SiteCharacteristics/other_characteristics.py
@@ -21,3 +21,10 @@ print(f"Alle spaceIDs site 2: {df_site2['spaceID'].unique()}")
 
 print(f"Anzahl Aufladungen Insgesamt site 1: {len(df_site1)}")
 print(f"Anzahl Aufladungen Insgesamt site 2: {len(df_site2)}")
+
+print(f"Komische spaceID: {df[df['spaceID'] == '11900388']}")
+print(f"Frühster Wert: {df[df['spaceID'] == '11900388']['connectionTime'].min()}")
+print(f"Spätester Wert: {df[df['spaceID'] == '11900388']['connectionTime'].max()}")
+
+print(f"Insgesamt frühster Wert: {df_site2['connectionTime'].min()}")
+print(f"Insgesamt frühster Wert: {df_site2['connectionTime'].max()}")
